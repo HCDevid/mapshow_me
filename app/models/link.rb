91d8551edc81845d7777083a8679369853f4b1c9
David Han <hcdevid@gmail.com>
2014-05-18 06:59:04 -0400

    Fixed missing http in out_url's causing weird navigation issues

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,26 @@
 class Link < ActiveRecord::Base
+  require 'uri'
   validates :in_url, :out_url, :http_status, :presence => true
   validates :in_url, :uniqueness => true
 
-  before_validation :generate_in_url
+  before_validation :generate_in_url, :clean_out_url
 
   private
 
     def generate_in_url
       self.in_url = SecureRandom.hex(5)
     end
+
+  def clean_out_url
+    uri = URI(self.out_url)
+    if uri.scheme.nil?
+      self.out_url = "http://#{self.out_url}"
+    else
+      return self.out_url
+    end
+  
+	end
+
+
+
 end
